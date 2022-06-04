2f12c4a04937b13fb3a8ff9d9fdfa49eedf03d6a
KristersgCode <kristersgutmanis@gmail.com>
2022-06-04 12:21:55 +0300

    git

--- public/script.js
+++ public/script.js
@@ -4,12 +4,26 @@ function calcAvarage() {
   let salaryThree = parseFloat(document.getElementById("calc-avarage3").value);
 
   let calcAvarages = (salaryOne + salaryTwo + salaryThree) / 3;
-  console.log(calcAvarages);
   document.getElementById("result").value = calcAvarages;
+
+  document.getElementById("0.75").value = calcAvarages * 0.75;
+
+  document.getElementById("0.5").value = calcAvarages * 0.5;
+
+  document.getElementById("0.45").value = calcAvarages * 0.45;
+
+  //validate input
+  // if (document.querySelectorAll(".alert").value.length == 0) {
+  //   alert("Please enter value for month 1");
+  //  }
 }
 
-function passvalues() {
-  let result = calcAvarage();
-  localStorage.setItem("textvalue", restult);
-  return false;
+function validateForm() {
+  let a = document.forms["Form"]["answer_a"].value;
+  let b = document.forms["Form"]["answer_b"].value;
+  let c = document.forms["Form"]["answer_c"].value;
+  if (!a || !b || !c) {
+    alert("Please Fill All Required Fields");
+    return false;
+  }
 }
